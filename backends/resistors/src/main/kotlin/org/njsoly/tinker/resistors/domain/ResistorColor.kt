2e5bfcec128702e953e7dfb5843cff2901364fac
njsoly <njsoly@syntj.com>
2026-04-09 15:44:49 -0500

    resistors: named enum parameters, renamed resource file

--- backends/resistors/src/main/kotlin/org/njsoly/tinker/resistors/domain/ResistorColor.kt
+++ backends/resistors/src/main/kotlin/org/njsoly/tinker/resistors/domain/ResistorColor.kt
@@ -2,7 +2,6 @@ package org.njsoly.tinker.resistors.domain
 
 import java.math.BigDecimal
 
-@Suppress("unused")
 enum class ResistorColor(
     val id: Int,
 
@@ -17,16 +16,16 @@ enum class ResistorColor(
     /** Tolerance given by the color when used in the tolerance band. */
     val tolerancePercent: Double?
 ) {
-    Black   (1, 0, BigDecimal(1), null),
-    Brown   (2, 1, BigDecimal(10), 1.0),
-    Red     (3, 2, BigDecimal(1E2), 2.0),
-    Orange  (4, 3, BigDecimal(1E3), .05),
-    Yellow  (5, 4, BigDecimal(1E4), .02),
-    Green   (6, 5, BigDecimal(1E5), .5),
-    Blue    (7, 6, BigDecimal(1E6), .25),
-    Violet  (8, 7, BigDecimal(1E7), .1),
-    Gray    (9, 8, BigDecimal(1E8), .01),
-    White   (10, 9, BigDecimal(1E9), null),
-    Gold    (11, 0, BigDecimal(1E-1), 5.0),
-    Silver  (12, 0, BigDecimal(1E-2), 10.0)
+    Black   (id = 1, significandValue = 0,  magnitudeMultiplier = BigDecimal(1),    tolerancePercent = null),
+    Brown   (id = 2, significandValue = 1,  magnitudeMultiplier = BigDecimal(10),   tolerancePercent = 1.0),
+    Red     (id = 3, significandValue = 2,  magnitudeMultiplier = BigDecimal(1E2),  tolerancePercent = 2.0),
+    Orange  (id = 4, significandValue = 3,  magnitudeMultiplier = BigDecimal(1E3),  tolerancePercent = .05),
+    Yellow  (id = 5, significandValue = 4,  magnitudeMultiplier = BigDecimal(1E4),  tolerancePercent = .02),
+    Green   (id = 6, significandValue = 5,  magnitudeMultiplier = BigDecimal(1E5),  tolerancePercent = .5),
+    Blue    (id = 7, significandValue = 6,  magnitudeMultiplier = BigDecimal(1E6),  tolerancePercent = .25),
+    Violet  (id = 8, significandValue = 7,  magnitudeMultiplier = BigDecimal(1E7),  tolerancePercent = .1),
+    Gray    (id = 9, significandValue = 8,  magnitudeMultiplier = BigDecimal(1E8),  tolerancePercent = .01),
+    White   (id = 10, significandValue = 9, magnitudeMultiplier = BigDecimal(1E9),  tolerancePercent = null),
+    Gold    (id = 11, significandValue = 0, magnitudeMultiplier = BigDecimal(1E-1), tolerancePercent = 5.0),
+    Silver  (id = 12, significandValue = 0, magnitudeMultiplier = BigDecimal(1E-2), tolerancePercent = 10.0)
 }
