cba42e410b7e10f868d8aada700cc381ce0f1785
njsoly <njsoly@syntj.com>
2026-04-09 19:35:31 -0500

    #4: Drop-down inputs for band color selection

--- frontends/tinker-react/src/components/ResistorFun.tsx
+++ frontends/tinker-react/src/components/ResistorFun.tsx
@@ -5,11 +5,29 @@ interface ResistorFunProps {
   bands: ResistorColor[];
 }
 
+function ResistorColorSelect(props: { name: string }) {
+  return (
+    <select name={props.name} >
+      {Object.values(ResistorColor).map((color) => (
+        <option key={color.name} value={color.name}>
+          {color.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
 export const ResistorFun = ({ bands }: ResistorFunProps) => {
   return (
     <>
       <div>
         <h1>Resistor Fun</h1>
+      </div>
+      <div>
+        <ResistorColorSelect name="first" />
+        <ResistorColorSelect name="second" />
+        <ResistorColorSelect name="third" />
+        <ResistorColorSelect name="fourth" />
       </div>
       <div>
         <ResistorDisplay bands={bands} />
