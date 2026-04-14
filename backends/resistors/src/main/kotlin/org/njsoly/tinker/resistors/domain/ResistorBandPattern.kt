1866e1758424755f8889a0cdadea66325a0302bc
njsoly <njsoly@syntj.com>
2026-04-13 22:32:41 -0500

    ResistorBandPattern: remove method, address nulls in response

--- backends/resistors/src/main/kotlin/org/njsoly/tinker/resistors/domain/ResistorBandPattern.kt
+++ backends/resistors/src/main/kotlin/org/njsoly/tinker/resistors/domain/ResistorBandPattern.kt
@@ -1,16 +1,12 @@
 package org.njsoly.tinker.resistors.domain
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class ResistorBandPattern(
     val band0: ResistorColor,
     val band1: ResistorColor,
     val band2: ResistorColor,
     val band3: ResistorColor? = null,
     val band4: ResistorColor? = null,
-) {
-    fun getListOfColors(): List<ResistorColor> {
-        val list = mutableListOf(band0, band1, band2)
-        band3?.let { list.add(it) }
-        band4?.let { list.add(it) }
-        return list
-    }
-}
+)
