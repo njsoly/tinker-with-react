2f192f65d7272834c32a5ae7b61dee930dac0a3e
njsoly <njsoly@syntj.com>
2026-04-16 10:42:34 -0500

    DrawringzA: resolve conflicts

--- frontends/tinker-react/src/components/DrawringzA.tsx
+++ frontends/tinker-react/src/components/DrawringzA.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-// noinspection JSUnusedLocalSymbols
+// noinspection JSUnusedLocalSymbols,ES6UnusedImports
 
 import { Arc, Circle, Line, Svg } from 'react-svg-path';
+// @ts-expect-error I intend to use this
+import { motion } from 'framer-motion';
 import './DrawringzA.scss';
 
 export const DrawringzA = () => {
@@ -18,16 +20,13 @@ export const DrawringzA = () => {
   // @ts-expect-error y aoeu
   const center: [number, number] = [width / 2, height / 2];
 
-
-
-
   return (
     <div className="canvassy">
       <p>hi</p>
-      <Svg width={200} height={100} x={4} y={4}>
-        <Circle cx={50} cy={50} r={45} size={100} />
+      <Svg width={200} height={100} x={4} y={4} fill={"none"} stroke={"darkCyan"}>
+        <Circle cx={50} cy={50} r={7} size={100} stroke={"purple"} />
         <Line x1={0} y1={0} x2={100} y2={100} ex={0} ey={0} />
-        <Arc sx={0} sy={0} rx={100} ry={100} ex={1} ey={10} />
+        <Arc sx={200} sy={100} rx={100} ry={100} ex={1} ey={10} stroke={"white"} />
         <i>Could not render SVG.</i>
       </Svg>
     </div>
